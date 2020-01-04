016030ce294326c8e35adc22c595f68a16f5fd74
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-04 14:31:28 -0500

    display array static function

--- javaplayground/src/main/java/javaplay/App.java
+++ javaplayground/src/main/java/javaplay/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        Arrays();
         System.out.println( "Hello World!" );
     }
 
@@ -20,6 +21,23 @@ public class App
         //create the array with a certian set of init values. the size of this set 
         // determines the size of the array -- it's exacly large enough to hold all 
         // of those values, its size is fixed for the life of the array.
+
+        /// new elementType arraySize;
+
+        //creates an empty array of 5 elements:
+        int[] integers = new int[5];
+        System.out.println(integers);
+
+        
+    }
+
+    public static void displayArray(int[] array) {
+
+        System.out.print("[ ");
+        for (int element : array) {
+            System.out.print(element);          
+        }
+        System.out.print(" ]");
 
         
     }
