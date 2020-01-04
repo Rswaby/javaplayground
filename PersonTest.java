9aa40fc6fb7a04c7fe8eed6f7c4899b2caa46d0b
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-03 19:46:57 -0500

    created instance

--- PersonTest.java
+++ PersonTest.java
@@ -1,12 +1,15 @@
+
 // package javaplay;
 import java.util.logging.Logger;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
 @Test
-public class PersonTest{
-    public void testPerson(){
-        
+public class PersonTest {
+    public void testPerson() {
+
+        Person p = new Person("Rohan Swaby", 11, 173, 82, "Red", "male");
+
     }
 }
-
