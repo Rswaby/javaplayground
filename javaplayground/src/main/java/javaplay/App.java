950e7ecfae8ccd12110ab6b88be4b385ebaf21ca
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-04 14:20:10 -0500

    notes: how arrays are created in java

--- javaplayground/src/main/java/javaplay/App.java
+++ javaplayground/src/main/java/javaplay/App.java
@@ -15,7 +15,12 @@ public class App
     //arrays review
 
     public static void Arrays() {
-        
+
+        //create the array with a certain size which is fixed for life of the array
+        //create the array with a certian set of init values. the size of this set 
+        // determines the size of the array -- it's exacly large enough to hold all 
+        // of those values, its size is fixed for the life of the array.
+
         
     }
 }
