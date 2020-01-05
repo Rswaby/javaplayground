c041becf5893e853d7bb3512be14e08bd6120e11
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-04 22:29:52 -0500

    added employee class

--- javaplayground/src/main/java/javaplay/Employee.java
+++ javaplayground/src/main/java/javaplay/Employee.java
@@ -1,7 +1,37 @@
 
 package javaplay;
 
-public class Employee extends Person{
+import java.math.BigDecimal;
+
+public class Employee extends Person {
+    private String taxpayerIdentificationNumber;
+    private String employeeNumber;
+    private BigDecimal salary;
+
+    public String getTaxpayerIdentificationNumber() {
+        return this.taxpayerIdentificationNumber;
+    }
+
+    public void setTaxpayerIdentificationNumber(String taxpayerIdentificationNumber) {
+        this.taxpayerIdentificationNumber = taxpayerIdentificationNumber;
+    }
+
+    public String getEmployeeNumber() {
+        return this.employeeNumber;
+    }
+
+    public void setEmployeeNumber(String employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public BigDecimal getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(BigDecimal salary) {
+        this.salary = salary;
+    }
+  
     public Employee(){
         super();
         // Auto‑generated constructor stub
