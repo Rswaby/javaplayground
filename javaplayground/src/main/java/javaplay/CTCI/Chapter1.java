564a95032a220f9056244d15f9e571064aaf53a4
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 16:37:52 -0500

    added some more problems

--- javaplayground/src/main/java/javaplay/CTCI/Chapter1.java
+++ javaplayground/src/main/java/javaplay/CTCI/Chapter1.java
@@ -3,9 +3,22 @@ package javaplay.CTCI;
 public class Chapter1{
 
 
-    public static void isUnique(String str) {
+    public static Boolean isUnique(String str) {
         //implement  
-        System.out.println(str);      
+        System.out.println(str);
+        
+        return true;
+    }
+
+    public static Boolean checkPermutation(String str1, String str2){
+
+        return true;
+    }
+
+
+    public static String URLify() {
+        
+        return "";
     }
 
 }
