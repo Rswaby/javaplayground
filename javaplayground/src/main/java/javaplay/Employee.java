a3b2642dd6162754b3396814e284f8e5b7deb9fc
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-04 22:19:56 -0500

    _

--- javaplayground/src/main/java/javaplay/Employee.java
+++ javaplayground/src/main/java/javaplay/Employee.java
@@ -2,5 +2,14 @@
 package javaplay;
 
 public class Employee extends Person{
-    
+    public Employee(){
+        super();
+        // Auto‑generated constructor stub
+    }
+
+    public Employee(String name, int age, int height, 
+    int weight, String eyeColor, String gender){
+        super(name, age, height, weight, eyeColor, gender);
+        // Auto-generted constructor
+    }
 }
