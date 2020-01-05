437b0b6b44f5654e95e7754025b272e7ad8cdd19
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 16:23:19 -0500

    removed constructor

--- javaplayground/src/main/java/javaplay/CTCI/Chapter1.java
+++ javaplayground/src/main/java/javaplay/CTCI/Chapter1.java
@@ -3,10 +3,6 @@ package javaplay.CTCI;
 public class Chapter1{
 
 
-    public Chapter1(){
-
-    }
-
     public static void isUnique(String str) {
         //implement  
         System.out.println(str);      
