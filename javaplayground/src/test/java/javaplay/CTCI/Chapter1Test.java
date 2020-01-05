8fedccae746d831f3b92ae40187838958dfcc746
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 16:21:50 -0500

    added chapter1

---
+++ javaplayground/src/test/java/javaplay/CTCI/Chapter1Test.java
@@ -0,0 +1,19 @@
+package javaplay.CTCI;
+
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+public class Chapter1Test{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void isUnqueTest()
+    {
+        assertTrue( true );
+    }
+}
