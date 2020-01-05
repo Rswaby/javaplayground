a2d5a4c7050f82298cd037290882e8738218a9bb
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 16:59:28 -0500

    isUnique done

--- javaplayground/src/main/java/javaplay/CTCI/Chapter1.java
+++ javaplayground/src/main/java/javaplay/CTCI/Chapter1.java
@@ -1,23 +1,35 @@
 package javaplay.CTCI;
 
-public class Chapter1{
+import java.util.HashSet;
+import java.util.Set;
 
+public class Chapter1 {
 
     public static Boolean isUnique(String str) {
-        //implement  
-        System.out.println(str);
-        
+        // implement
+
+        Set<Character> unique = new HashSet<>();
+        for (char character : str.toCharArray()) {
+            Character ch = Character.valueOf(character);
+            if (unique.contains(ch)) {
+                return false;
+            }else{
+                unique.add(ch);
+            }
+
+        }
+        //System.out.println("");
+
         return true;
     }
 
-    public static Boolean checkPermutation(String str1, String str2){
+    public static Boolean checkPermutation(String str1, String str2) {
 
         return true;
     }
-
 
     public static String URLify() {
-        
+
         return "";
     }
 
