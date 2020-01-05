109586b1b35585f3294fc17052272a05543be350
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 16:51:13 -0500

    charset

--- javaplayground/src/main/java/javaplay/App.java
+++ javaplayground/src/main/java/javaplay/App.java
@@ -20,7 +20,7 @@ public class App {
         ArrList();
         setOfStuff();
         System.out.println("Hello World!");
-        Chapter1.isUnique("hello");
+        Chapter1.isUnique("");
     }
     //Maps
 
