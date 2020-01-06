8ba4061c9d1d770250222ed8a8e561876ad47a32
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-06 13:28:21 -0500

    preorder

--- javaplayground/src/main/java/javaplay/CTCI/Chapter4.java
+++ javaplayground/src/main/java/javaplay/CTCI/Chapter4.java
@@ -14,9 +14,17 @@ public class Chapter4 {
 
     public static void preOrder(TreeNode root){
         if(root != null){
-            System.out.println(root.val);
+            System.out.print(root.val);
             preOrder(root.left);
             preOrder(root.right);
+        }
+    }
+
+    public static void postOrder(TreeNode root){
+        if(root != null){
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.val);
         }
     }
 
