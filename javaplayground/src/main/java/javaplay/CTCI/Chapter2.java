3fda2c31ae1ce7cfbd6d6f90c9ebd384c982a558
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 20:05:29 -0500

    remove node

--- javaplayground/src/main/java/javaplay/CTCI/Chapter2.java
+++ javaplayground/src/main/java/javaplay/CTCI/Chapter2.java
@@ -3,4 +3,21 @@ package javaplay.CTCI;
 
 public class Chapter2{
 
+    public static Node deleteNode(Node head, int target) {
+
+        if (head.val == target) return head.next;
+        Node curr = head;
+
+        while(curr.next != null){
+            if (curr.val ==  target){
+                curr.next = curr.next.next;
+                return head;
+            }
+        }
+        return head;
+        
+    }
+
+    
+
 }
