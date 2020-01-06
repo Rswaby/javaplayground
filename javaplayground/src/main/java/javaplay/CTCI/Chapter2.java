0f54cfa9de973105e72849b2768c260798a97758
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-05 20:34:02 -0500

    remove duplicates

--- javaplayground/src/main/java/javaplay/CTCI/Chapter2.java
+++ javaplayground/src/main/java/javaplay/CTCI/Chapter2.java
@@ -1,6 +1,9 @@
 package javaplay.CTCI;
 // linked List
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Chapter2{
 
     public static Node deleteNode(Node head, int target) {
@@ -18,6 +21,21 @@ public class Chapter2{
         
     }
 
-    
+    public static Node removeDuplicates(Node head){
+
+        Set<Integer> buff = new HashSet<>();
+        Node curr = head;
+        while(curr.next != null){
+            if (buff.contains(curr.val)){
+                curr.next = curr.next.next;
+            }else{
+                buff.add(curr.val);
+            }
+        }
+        return head;
+
+    }
+
+
 
 }
